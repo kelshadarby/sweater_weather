4176b567d4106959cb1272a3b48e4acf986b972d
Kelsha_Darby <ohanaipo@gmail.com>
2020-06-08 10:04:36 -0600

    Add zomato call

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -10,13 +10,38 @@ class Api::V1::FoodieController < ApplicationController
     duration = parsed_trip_response[:routes][0][:legs][0][:duration][:text]
 
     # Forecast Data
+    json = ForecastSerializer.new(get_forecast_object).serialized_json
+    parsed = JSON.parse(json, symbolize_names: true)
+
+    forecast_temp = parsed[:data][:attributes][:actual_temp]
+    forecast_deswcription = parsed[:data][:attributes][:weather_description]
+
+    # Geocode Lat Lon
+    obj = (GeocodingService.new).get_geocode_objects(params[:end])
+    lat = obj.latitude
+    lng = obj.longitude
+    # lat = 38.2544472
+    # long = -104.6091409
+
+
+    # Zomato API
+    zomato_response = Faraday.get("https://developers.zomato.com/api/v2.1/search") do |f|
+      f.headers[:user_key] = ENV["ZOMATO_API_KEY"]
+      f.params[:lat] = lat
+      f.params[:lon] = lng
+      f.params[:q] = params[:search]
+    end
+
+    parsed_zomato_response = JSON.parse(zomato_response.body, symbolize_names: true)
+
+    name = parsed_zomato_response[:restaurants][0][:restaurant][:name]
+    address = parsed_zomato_response[:restaurants][0][:restaurant][:location][:address]
+  end
+
+  private
+
+  def get_forecast_object
     forecast_service = ForecastService.new(params[:end])
-    obj = forecast_service.get_forecast_objects
-    json = ForecastSerializer.new(get_forecast_object).to_
-
-
-    require "pry"; binding.pry
-    obj[:current]
-
+    forecast_service.get_forecast_objects
   end
 end
