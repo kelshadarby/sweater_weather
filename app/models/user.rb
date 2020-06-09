e860453fa7e01c292fbf8658af1e229a8caa4057
Kelsha_Darby <ohanaipo@gmail.com>
2020-06-08 22:01:34 -0600

    Add edge case testing and refactor api_key creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,6 @@ class User < ApplicationRecord
   has_secure_password
 
   def generate_unique_api_key
-    key = generate_api_key
-    if User.find_by(api_key: key)
-      until !(User.find_by(api_key: key)) do
-        new_key = generate_api_key
-      end
-      self.api_key = new_key
-    else
-      self.api_key = key
-    end
-  end
-
-  private
-
-  def generate_api_key
-    Array.new(27){[*"A".."Z", *"a".."z", *"0".."9"].sample}.join
+    self.api_key = SecureRandom.hex
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,20 @@ RSpec.describe User, type: :model do
 
       user_2 = User.create(email: 'email2@example.com')
       expect(user_2.api_key).to_not eq(user.api_key)
+
+      user_3 = User.create(email: 'email3@example.com',
+                           password: "password",
+                           api_key: '08374d9df11ad1baf159d4f63ed80c61')
+
+      expect(User.last).to eq(user_3)
+      expect{User.find(user_3.id)}.to_not raise_error(ActiveRecord::RecordNotFound)
+
+      user_4 = User.create(email: 'email4@example.com',
+                           password: "password",
+                           api_key: '08374d9df11ad1baf159d4f63ed80c61')
+
+      expect{User.find(user_4.id)}.to_not raise_error(ActiveRecord::RecordNotFound)
+      expect(user_4.api_key).to_not eq('08374d9df11ad1baf159d4f63ed80c61')
     end
   end
 end
