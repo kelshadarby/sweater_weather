c6af84ca1f3c84176178af56a8279d0d66b5f043
Kelsha_Darby <ohanaipo@gmail.com>
2020-06-08 22:36:32 -0600

    Add to road trip controller to handle errors

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::RoadTripController < ApplicationController
   def show
-    render json: RoadTripSerializer.new(road_trip_facade)
+    user = User.find_by(api_key: params[:road_trip][:api_key])
+    if user
+      render json: RoadTripSerializer.new(road_trip_facade)
+    elsif !(params[:road_trip][:api_key])
+      render json: { errors: "API Key is Required" }, status: 401
+    else
+      render json: { errors: "Invalid API Key" }, status: 401
+    end
   end
 
   private
